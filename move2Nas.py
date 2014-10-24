f5f759bd73b103692f15bff0529bc6fb66deecea
jimbo1969 <james_wing1@hotmail.com>
2014-10-24 11:16:01 -0600

    Turned off confirm flag on put() call.  Seems to have resolved system lockup upon wireless network failure mid-upload.

--- move2Nas.py
+++ move2Nas.py
@@ -147,7 +147,8 @@ def main(host, user, password, source_folder, destination_folder='', **kwargs):
                     break
     finally:
         logging.info('closing sftp connection')
-        s.close()
+        if s is not None:
+            s.close()
 
 
 if __name__ == "__main__":
